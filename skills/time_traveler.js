e8f2142cdc071809d3faf73d7e0504fed8277128
Glitch (isaacwang4076-shipit) <none>
2018-07-27 09:13:21 +0000

    🍲🐃 Checkpoint ./skills/time_traveler.js:1506805/55

--- skills/time_traveler.js
+++ skills/time_traveler.js
@@ -4,7 +4,7 @@ module.exports = function(controller) {
 	var timelines = ['../time/start_conversation.js', '../time/post_to_aotd.js', '../time/matching_meetups.js']
 	var timeline_index = 0;
 
-    controller.hears(['timetravel'], 'direct_message,direct_mention', function(bot, message) {
+    controller.hears(['timetravel', 'time travel'], 'direct_message,direct_mention', function(bot, message) {
     	console.log("Zipping to timeline number " + (timeline_index + 1));
     	require(timelines[timeline_index])(controller);
     	timeline_index = (timeline_index + 1) % timelines.length ;
