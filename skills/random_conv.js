0a6fd087372b53f0bab1a769cb7c50d2693cebcd
Glitch (isaacwang4076-shipit) <none>
2018-07-27 10:48:16 +0000

    😺🏑 Checkpoint ./skills/random_conv.js:1506805/1918

--- skills/random_conv.js
+++ skills/random_conv.js
@@ -1,3 +1,9 @@
+var fs = require('fs');
+var path = require('path');
+var jsonPath = path.join(__dirname, '..', 'questions.txt');
+var jsonString = fs.readFileSync(jsonPath, 'utf8');
+var questions = jsonString.split("\n");
+
 module.exports = function(controller) {
 
     controller.hears(['awkward'], 'direct_message, direct_mention', function(bot, message) {
@@ -5,14 +11,19 @@ module.exports = function(controller) {
             convo.say(get_random_question());
         });
     });
+    controller.hears(['keep em comin'], 'direct_message, direct_mention', function(bot, message) {
+        var interval_id = setInterval(function() {
+          bot.startConversation(message, function(err, convo) {
+              convo.say(get_random_question());
+          });
+        }, 2000);
+        setTimeout(function() {
+          clear
+        });
+    });
 }
 
 function get_random_question() {
-	var fs = require('fs');
-  var path = require('path');
-  var jsonPath = path.join(__dirname, '..', 'questions.txt');
-  var jsonString = fs.readFileSync(jsonPath, 'utf8');
-  var questions = jsonString.split("\n");
   var question = questions[Math.floor(Math.random()*questions.length)];
   return question
 }
